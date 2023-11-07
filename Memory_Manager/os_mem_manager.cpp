c9d02f708c203fc774a54d6dad99e65992d5ef9b
ariceew <dendynavi9@gmail.com>
2023-11-07 09:35:35 +0500

    Memory_Manager
    with Tim's code

---
+++ Memory_Manager/os_mem_manager.cpp
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include "os_mem.h"   
+
+//***********************************
+
+int global_size = 0; // Глобальная переменная - размерностью всей памяти с которой будет производиться работа
+
+int global_Note_size = 0; // Глобальная переменная, отвечающая за размерность массива структур в данное время
+
+typedef struct Node {
+    int addr;
+    int size;
+}Node;
+
+Node* array = NULL;
+
+//***********************************
+
+int my_create(int size, int num_pages);
+
+void realloc_array(int counter);
+
+int my_destroy();
+
+void realloc_array(int counter);
+
+int my_destroy();
+
+int my_get_max_block_size();
+
+mem_handle_t my_alloc(int block_size);
+
+int my_get_free_space();
+
+void my_print_blocks();
+
+int my_free(mem_handle_t h);
+
+mem_handle_t my_get_block(int addr, int size);
+
+void setup_memory_manager(memory_manager_t* mm);
+
+//***********************************
+
+int my_create(int size, int num_pages) // создание менеджера памяти
+{
+    if (size > 0 && global_size == 0)
+    {
+        global_size = size;
+        return 1;
+    }
+    return 0; 
+}
+
+void realloc_array(int counter) // функция которая реаллочит массив структур, основываясь на поданный размер (проверку мб дописать хз)
+{
+    if (array == NULL)
+    {
+        array = (Node*)malloc(global_Note_size * sizeof(Node));
+        return;
+    }
+
+    Node* tmp_array = (Node*)malloc(global_Note_size * sizeof(Node));
+
+    for (int i = 0; i < counter; i++)
+    {
+        tmp_array[i].addr = array[i].addr;
+        tmp_array[i].size = array[i].size;
+    }
+
+    free(array);
+
+    array = tmp_array;
+}
+
+int my_destroy() // функция удаления массива структур -> удаления менеджера памяти
+{
+    if (global_size == 0) return 0;
+    free(array);
+    array = NULL;
+    global_Note_size = 0;
+    global_size = 0;
+    return 1;
+}
+
+int my_get_max_block_size() // функция нахождения самого большого свободного блока (использую в последующем в alloc)
+{
+    int counter = 0, tmp = 0;
+    if (global_Note_size > 0) {
+
+        tmp = global_size - (array[global_Note_size - 1].addr + array[global_Note_size - 1].size);
+
+        for (int i = 0; i < global_Note_size - 1; i++)
+        {
+            counter = array[i + 1].addr - (array[i].addr + array[i].size);
+
+            if (counter >= tmp)
+            {
+                tmp = counter;
+            }
+        }
+        return tmp;
+    }
+
+    return global_size;
+}
+
+mem_handle_t my_alloc(int block_size) // функция добавление блока
+{
+    if (global_Note_size == 0)
+    {
+        global_Note_size++;
+        realloc_array(global_Note_size);
+        array[0].addr = 0;
+        array[0].size = block_size;
+        return { array[0].addr ,array[0].size };
+    }
+    else
+    {
+        int counter = 0, tmp = 0, size_for_block, i = 0, result_addr, result_size;
+        size_for_block = my_get_max_block_size();
+        if (size_for_block >= block_size)
+        {
+            tmp = size_for_block;
+            global_Note_size += 1;
+            realloc_array(global_Note_size - 1); // так как в цикле я бегу по переданной переменной, а выделяю память global_Note_size 
+
+            for (i = 0; i < global_Note_size - 2; i++)
+            {
+                counter = array[i + 1].addr - (array[i].addr + array[i].size); 
+
+                if (counter >= tmp)
+                {
+                    tmp = counter;
+                    if (tmp == size_for_block)
+                    {
+                        int k = i + 1, x = 1, y = 2;
+                        for (k; k < global_Note_size - 1; k++)
+                        {
+                            array[global_Note_size - x].addr = array[global_Note_size - y].addr;
+                            array[global_Note_size - x].size = array[global_Note_size - y].size;
+                            x++;
+                            y++;
+                        }
+                        array[i + 1].addr = array[i].addr + array[i].size;
+                        array[i + 1].size = block_size;
+                        return { array[i + 1].addr,array[i + 1].size };
+                    }
+                }
+            }
+            array[global_Note_size - 1].addr = array[global_Note_size - 2].addr + array[global_Note_size - 2].size;
+            array[global_Note_size - 1].size = block_size;
+            return { array[global_Note_size - 1].addr,array[global_Note_size - 1].size };
+        }
+    }
+    return{ 0,0 }; 
+}
+
+int my_get_free_space() // функция определения всей оставшейся памяти в байтах
+{
+    if (global_Note_size == 0) return global_size;
+    int counter = 0, tmp = 0;
+    tmp = global_size - (array[global_Note_size - 1].addr + array[global_Note_size - 1].size);
+    for (int i = 0; i < global_Note_size - 1; i++)
+    {
+        counter = array[i + 1].addr - (array[i].addr + array[i].size);
+        tmp += counter;
+    }
+    return tmp;
+}
+
+void my_print_blocks() // функция записи блоков (предположительно в консоль)
+{
+    for (int i = 0; i < global_Note_size; i++)
+    {
+        if (array[i].addr == 0 && array[i].size == 0) continue;
+        printf("%d %d\n", array[i].addr, array[i].size);
+    }
+}
+
+int my_free(mem_handle_t h) // Удаление блока памяти с заданным адресом и длиной (для меня же, удаление элемента из массива, сдвиг массива, реаллок массива)
+{
+    if (h.addr == 0 && h.size == array[0].size)
+    {
+        array[0].size = 0;
+        return 1;
+    }
+
+    for (int i = 0; i < global_Note_size; i++)
+    {
+        if (array[i].addr == h.addr)
+        {
+            if (array[i].size == h.size)
+            {
+                for (i; i < global_Note_size - 1; i++)
+                {
+                    array[i].addr = array[i + 1].addr;
+                    array[i].size = array[i + 1].size;
+                }
+                global_Note_size--;
+                realloc_array(global_Note_size);
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+mem_handle_t my_get_block(int addr, int size) // функция возвращает дескриптор по переданным в нее параметрам
+{
+    for (int i = 0; i < global_Note_size; i++)
+    {
+        if (array[i].addr <= addr && (array[i].addr + array[i].size) >= addr + size)
+        {
+            return { array[i].addr, array[i].size };
+        }
+    }
+    return { 0,0 };
+}
+
+void setup_memory_manager(memory_manager_t* mm) {
+    mm->create = my_create; 
+    mm->destroy = my_destroy; 
+    mm->alloc = my_alloc; 
+    mm->get_max_block_size = my_get_max_block_size; 
+    mm->get_free_space = my_get_free_space; 
+    mm->print_blocks = my_print_blocks; 
+    mm->free = my_free; 
+}
---
+++ TEMIRKHON/08_2_08task.cpp
@@ -0,0 +1,272 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+#include "os_mem.h"
+#pragma warning(disable:4996)
+
+typedef struct element
+{
+	char name[32];
+	int numb_of_links;
+	element* links[16];
+
+}element;
+
+element* ARR = NULL;
+int NUM_OBJ_MAX = 0;
+int NUM_OBJ_CURR = 0;
+// Инициализация вспомогательных функций
+void copy_plus_vibe(element* destination, element* old_arr); // очень вайбовая фукнция, спасла весь код
+
+int checkMemoryManagerAvilability(int new_elem); // делает основные проверки
+
+void sort_strings_in_element(element* arr, int size); // сортирует строки 
+
+void good_realloc(int new_size, int old_size); // нормальный реаллок без утечек (в теории)
+
+int find_element(element* arr, const char* name, int arr_size); // возвращает индекс элемента
+
+void destroy_elem_and_shift_array(int index, int size); // удаляет с массива элемент
+// ************************************
+
+
+// Инициализация моих функций
+int my_create(int size, int num_pages);
+
+int my_destroy();
+
+int my_create_object(const char* name);
+
+int my_destroy_object(const char* name);
+
+void my_print_objects();
+
+int my_link(const char* object1_name, const char* object2_name);
+
+void print_link_counts();
+// ************************************
+// Инициализация мемори менеджера
+
+void setup_memory_manager(memory_manager_t* mm) {
+    mm->create = my_create; 
+    mm->destroy = my_destroy; 
+	mm->create_object = my_create_object;
+	mm->destroy_object = my_destroy_object;
+	mm->print_objects = my_print_objects;
+	mm->link = my_link;
+	mm->print_link_counts = print_link_counts;
+}
+
+// ************************************
+
+// мейн ого
+// ************************************
+
+// Работа вспомогательных функций
+
+void copy_plus_vibe(element* destination, element* old_arr)
+{
+	for (int j = 0; j < old_arr->numb_of_links; j++)
+	{
+		destination->links[j] = old_arr->links[j];
+	}
+	strcpy(destination->name, old_arr->name);
+	destination->numb_of_links = old_arr->numb_of_links;
+}
+
+int checkMemoryManagerAvilability(int new_elem)
+{
+	return ((NUM_OBJ_MAX) && (NUM_OBJ_CURR + new_elem <= NUM_OBJ_MAX));
+}
+
+void good_realloc(int new_size, int old_size)
+{
+	if (!old_size)
+	{
+		// Если создается первый элемент
+		ARR = (element*)malloc(sizeof(element));
+		return;
+	}
+	/*element* new_arr = (element*)malloc(sizeof(element) * old_size);
+	for (int i = 0; i < old_size; i++)
+	{
+		copy_plus_vibe(&new_arr[i], &ARR[i]);
+	}
+	free(ARR);
+
+	ARR = (element*)malloc(sizeof(element) * new_size);
+	for (int i = 0; i < old_size; i++)
+	{
+		copy_plus_vibe(&ARR[i], &new_arr[i]);
+	}
+	free(new_arr);*/
+	element* new_arr = (element*)malloc(sizeof(element) * new_size);
+	for (int i = 0; i < old_size; i++)
+	{
+		copy_plus_vibe(&new_arr[i], &ARR[i]);
+	}
+	free(ARR);
+	ARR = new_arr;
+}
+
+void sort_strings_in_element(element* arr, int size) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (strcmp(arr[i].name, arr[j].name) > 0) {
+				// Если текущая строка больше следующей, меняем их местами
+				element temp;
+				copy_plus_vibe(&temp, &arr[i]);
+				copy_plus_vibe(&arr[i], &arr[j]);
+				copy_plus_vibe(&arr[j], &temp);
+			}
+		}
+	}
+}
+
+int find_element(element* arr, const char* name, int arr_size)
+{
+	int i = 0;
+	for (; i < arr_size; i++)
+	{
+		if (!strcmp(arr[i].name, name))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void destroy_elem_and_shift_array(int index, int size)
+{
+	int shift = 0;
+	int new_size = size - 1;
+	element* new_arr = (element*)malloc(sizeof(element) * (new_size));
+	for (int i = 0; i < size; i++)
+	{
+		if (i == index)
+		{
+			shift++;
+			element* link = ARR[i].links[0];
+			int j = 0; // check
+			while (link)
+			{
+				link[j].numb_of_links--;
+				j++;
+			}
+			continue;
+		}
+		copy_plus_vibe(&new_arr[i], &ARR[i]);
+	}
+	free(ARR);
+	ARR = (element*)malloc(sizeof(element) * (new_size));
+	for (int i = 0; i < new_size; i++)
+	{
+		copy_plus_vibe(&ARR[i], &new_arr[i]);
+	}
+	free(new_arr);
+}
+// ************************************
+
+// Работа моих функций
+int my_create(int size, int num_pages)
+{
+	if (NUM_OBJ_MAX) return 0;
+	NUM_OBJ_MAX = size;
+	return 1;
+}
+
+int my_destroy()
+{
+	if (!checkMemoryManagerAvilability(0)) return 0;
+	NUM_OBJ_MAX = 0;
+	free(ARR);
+	return 1;
+}
+
+int my_create_object(const char* name)
+{
+	if (!checkMemoryManagerAvilability(1)) return 0;
+	int new_size = NUM_OBJ_CURR + 1;
+	good_realloc(new_size, NUM_OBJ_CURR);
+	for (int i = 0; i < 16; i++)
+	{
+		ARR[new_size - 1].links[i] = NULL;
+	}
+	ARR[new_size - 1].numb_of_links = 0;
+	/*for (int i = 0; i < lenght; i++)
+	{
+		ARR[new_size - 1].name[i] = name[i];
+	}*/
+	strcpy(ARR[new_size - 1].name, name);
+	NUM_OBJ_CURR++;
+	return 1;
+}
+
+int my_destroy_object(const char* name)
+{
+	if (!checkMemoryManagerAvilability(0)) return 0;
+	int elem_index = find_element(ARR, name, NUM_OBJ_CURR);
+	if (elem_index == -1) return 0;
+	destroy_elem_and_shift_array(elem_index, NUM_OBJ_CURR);
+	NUM_OBJ_CURR--;
+	return 1;
+}
+
+int my_link(const char* object1_name, const char* object2_name)
+{
+	if (!checkMemoryManagerAvilability(0)) return 0;
+	int parent_index = find_element(ARR, object1_name, NUM_OBJ_CURR);
+	int child_index = find_element(ARR, object2_name, NUM_OBJ_CURR);
+	if ((parent_index == -1) || (child_index == -1)) return 0;
+	//element* link = ARR[parent_index].links[0]; // check
+	//int j = 0;
+	//while (link)
+	//{
+	//	j++;
+	//	link = ARR[parent_index].links[j];
+	//}
+	//ARR[parent_index].links[j] = &ARR[child_index];
+	//if (j < 16)
+	//{
+	//	ARR[parent_index].links[j] = NULL;
+	//}
+	element* link = ARR[parent_index].links[0]; // Начнем с первого элемента
+	int j = 0;
+	while (link) 
+	{
+		j++;
+		if (j >= 16) 
+		{
+			// В массиве links не осталось доступных мест
+			break;
+		}
+		link = ARR[parent_index].links[j];
+	}
+	if (j < 16) 
+	{
+		ARR[parent_index].links[j] = &ARR[child_index]; // Установить на ARR[child_index]
+	}
+	ARR[child_index].numb_of_links++;
+	return 1;
+}
+
+void my_print_objects()
+{
+	if (!checkMemoryManagerAvilability(0)) return;
+	sort_strings_in_element(ARR, NUM_OBJ_CURR);
+	for (int i = 0; i < NUM_OBJ_CURR; i++)
+	{
+		printf("%s\n", ARR[i].name);
+	}
+}
+
+void print_link_counts()
+{
+	if (!checkMemoryManagerAvilability(0)) return;
+	sort_strings_in_element(ARR, NUM_OBJ_CURR);
+	for (int i = 0; i < NUM_OBJ_CURR; i++)
+	{
+		printf("%s,%d\n", ARR[i].name, ARR[i].numb_of_links);
+	}
+}
+// ************************************
